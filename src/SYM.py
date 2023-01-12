aa8e3aeb24b1e20aac0d3fe2bcb2b00210aa56b5
ashishjoshi2605 <ashishjoshi2605@gmail.com>
2023-01-12 15:31:35 -0500

    adding functionalities in SYM

---
+++ src/SYM.py
@@ -0,0 +1,32 @@
+import math
+class SYM:
+    def __init__(self) -> None:
+        self.n = 0
+        self.has = dict()
+        self.most = 0
+        self.mode = None
+
+    def add(self, x) -> None:
+        if x != '?':
+            self.n += 1
+            if x in self.has:
+                self.has[x] += 1
+            else:
+                self.has[x] = 1
+            if self.has[x] > self.most:
+                self.most, self.mode = self.has[x], x
+            
+
+    def mid(self, x) -> str:
+        return self.mode
+
+    def div(self, x) -> float:
+        
+        def fun(p):
+            return p*math.log(p,2)
+        
+        e = 0
+        for _, n in enumerate(self.has):
+            e += fun(n/self.n)
+            
+        return -e
